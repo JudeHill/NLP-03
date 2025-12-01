c72af5d5e4429518213ee7bc83a86cb633d06b7b
Evoshrike <169169729+Evoshrike@users.noreply.github.com>
2025-12-01 01:59:45 +0000

    scripts

---
+++ xpos_script.bat
@@ -0,0 +1,3 @@
+python -m main train-test hmm-EM xpos --max-epochs 9 5 --save-path ./xpos_save/EM/hmm_45.pt --res-path ./xpos_res/EM/9_5.csv
+python -m main train-test hmm-hardEM xpos --max-epochs 9 5 --save-path ./xpos_save/hardEM/hmm_45.pt --res-path ./xpos_res/hardEM/9_5.csv
+python -m main train-test hmm-sEM xpos --max-epochs 9 5 --save-path ./xpos_save/sEM/hmm_45.pt --res-path ./xpos_res/sEM/9_5.csv
